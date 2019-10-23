6f87f2344d3051edbdd55ee23dada6fd6f748d2e
mei23 <m@m544.net>
2019-10-23 23:37:46 +0900

    Revert "syuilo#5510 imgをattachment扱いにするけどあんまり入れる気ない"
    This reverts commit 06ac5ea96e3c7aa3b1e47ff75739975cb7979ad0.

--- src/mfm/fromHtml.ts
+++ src/mfm/fromHtml.ts
@@ -2,26 +2,18 @@ import { parseFragment, DefaultTreeDocumentFragment } from 'parse5';
 import { URL } from 'url';
 import { urlRegex } from './prelude';
 
-export function fromHtml(html: string) {
-	return fromHtml2(html).text;
-}
-
-export function fromHtml2(html: string) {
+export function fromHtml(html: string): string {
 	if (html == null) return null;
 
 	const dom = parseFragment(html) as DefaultTreeDocumentFragment;
 
 	let text = '';
-	const imgs: string[] = [];
 
 	for (const n of dom.childNodes) {
 		analyze(n);
 	}
 
-	return {
-		text: text.trim(),
-		imgs
-	};
+	return text.trim();
 
 	function getText(node: any): string {
 		if (node.nodeName == '#text') return node.value;
@@ -77,11 +69,6 @@ export function fromHtml2(html: string) {
 						analyze(n);
 					}
 				}
-				break;
-
-			case 'img':
-				const src = node.attrs.find((x: any) => x.name == 'src');
-				if (src && src.value) imgs.push(src.value);
 				break;
 
 			default:
