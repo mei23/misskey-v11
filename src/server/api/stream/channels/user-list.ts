741fe6963d738c6ba4c10a548f9e8db6b642906e
mei23 <m@m544.net>
2019-10-26 20:34:29 +0900

    Fix: リストにミュートが効かない

--- src/server/api/stream/channels/user-list.ts
+++ src/server/api/stream/channels/user-list.ts
@@ -1,15 +1,21 @@
 import autobind from 'autobind-decorator';
 import Channel from '../channel';
 import { pack } from '../../../../models/note';
+import Mute from '../../../../models/mute';
+import shouldMuteThisNote from '../../../../misc/should-mute-this-note';
 
 export default class extends Channel {
 	public readonly chName = 'userList';
 	public static shouldShare = false;
 	public static requireCredential = false;
 
+	private mutedUserIds: string[] = [];
+
 	@autobind
 	public async init(params: any) {
 		const listId = params.listId as string;
+		const mute = this.user ? await Mute.find({ muterId: this.user._id }) : null;
+		this.mutedUserIds = mute ? mute.map(m => m.muteeId.toString()) : [];
 
 		// Subscribe stream
 		this.subscriber.on(`userListStream:${listId}`, async data => {
@@ -19,6 +25,9 @@ export default class extends Channel {
 			});
 
 			if (data.body && data.body.isHidden) return;
+
+			// 流れてきたNoteがミュートしているユーザーが関わるものだったら無視する
+			if (data.type == 'note' && data.body && shouldMuteThisNote(data.body, this.mutedUserIds)) return;
 
 			this.send(data);
 		});
