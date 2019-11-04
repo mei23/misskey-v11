fe327b2b9fd4667fa2904ee6fb499ca3f3513e56
mei23 <m@m544.net>
2019-11-04 20:59:15 +0900

    ジョブキューageは最小ミリ秒に

--- src/queue/get-job-info.ts
+++ src/queue/get-job-info.ts
@@ -2,11 +2,14 @@ import * as Bull from 'bull';
 
 export function getJobInfo(job: Bull.Job, increment = false) {
 	const age = Date.now() - job.timestamp;
-	const ageMinutes = Math.floor(age / 1000 / 60);
+
+	const formated = age > 60000 ? `${Math.floor(age / 1000 / 60)}m`
+		: age > 10000 ? `${Math.floor(age / 1000)}s`
+		: `${age}ms`;
 
 	// onActiveとかonCompletedのattemptsMadeがなぜか0始まりなのでインクリメントする
 	const currentAttempts = job.attemptsMade + (increment ? 1 : 0);
 	const maxAttempts = job.opts ? job.opts.attempts : 0;
 
-	return `id=${job.id} attempts=${currentAttempts}/${maxAttempts} age=${ageMinutes}m`;
+	return `id=${job.id} attempts=${currentAttempts}/${maxAttempts} age=${formated}`;
 }
