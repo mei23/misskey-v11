dd804d7d3102ff89de1fbd52ac56db2fd8030e08
mei23 <m@m544.net>
2019-11-24 06:56:29 +0900

    tool clean old renotes

--- cli/_clean_old_renotes.js
+++ cli/_clean_old_renotes.js
@@ -58,7 +58,10 @@ async function main() {
                 },
                 {
                     renoteId: { $ne: null },
-                },
+								},
+								{
+									text: null,
+								}
             ],
         });
         for (const note of notes) {
