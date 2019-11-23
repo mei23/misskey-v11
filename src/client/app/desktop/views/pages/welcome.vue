cd479608f942b004c8a63a70ccd92a9b5f4190e1
mei23 <m@m544.net>
2019-11-23 15:18:34 +0900

    Revert "remove trends dsktop"
    This reverts commit bb36c4f5c831d1328f0435a86e06d32b8d7414d4.

--- src/client/app/desktop/views/pages/welcome.vue
+++ src/client/app/desktop/views/pages/welcome.vue
@@ -56,6 +56,12 @@
 			</div>
 
 			<div class="side">
+				<div class="trends block">
+					<div>
+						<mk-trends/>
+					</div>
+				</div>
+
 				<div class="tl block">
 					<header><fa :icon="['far', 'comment-alt']"/> {{ $t('@.featured-notes') }}</header>
 					<div>
@@ -418,9 +424,14 @@ export default Vue.extend({
 				gap 16px
 
 				> .tl
-					grid-row 1/3
+					grid-row 1
 					grid-column 1
 					overflow auto
+
+				> .trends
+					grid-row 2
+					grid-column 1
+					padding 8px
 
 				> .info
 					grid-row 3
