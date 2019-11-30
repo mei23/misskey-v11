716a9e6784b625b2f917c50f41615ce0800e29bb
mei23 <m@m544.net>
2019-11-30 17:13:23 +0900

    クライアント側のmeta更新間隔を5分に

--- src/client/app/mios.ts
+++ src/client/app/mios.ts
@@ -484,7 +484,7 @@ export default class MiOS extends EventEmitter {
 				return;
 			}
 
-			const expire = 1000 * 60 * 1;
+			const expire = 1000 * 60 * 5;
 
 			// forceが有効, meta情報を保持していない or 期限切れ
 			if (force || this.meta == null || Date.now() - this.meta.chachedAt.getTime() > expire) {
