04dffa1d5b9b887d3ee374a7c1abbc669061b7cf
mei23 <m@m544.net>
2019-12-03 12:51:19 +0900

    API permissionのv11互換等

--- src/client/app/dev/views/new-app.vue
+++ src/client/app/dev/views/new-app.vue
@@ -15,15 +15,22 @@
 				<b-form-group :description="$t('description')">
 					<b-alert show variant="warning"><fa icon="exclamation-triangle"/> {{ $t('authority-warning') }}</b-alert>
 					<b-form-checkbox-group v-model="permission" stacked>
-						<b-form-checkbox value="account-read">{{ $t('account-read') }}</b-form-checkbox>
-						<b-form-checkbox value="account-write">{{ $t('account-write') }}</b-form-checkbox>
-						<b-form-checkbox value="note-write">{{ $t('note-write') }}</b-form-checkbox>
-						<b-form-checkbox value="reaction-write">{{ $t('reaction-write') }}</b-form-checkbox>
-						<b-form-checkbox value="following-write">{{ $t('following-write') }}</b-form-checkbox>
-						<b-form-checkbox value="drive-read">{{ $t('drive-read') }}</b-form-checkbox>
-						<b-form-checkbox value="drive-write">{{ $t('drive-write') }}</b-form-checkbox>
-						<b-form-checkbox value="notification-read">{{ $t('notification-read') }}</b-form-checkbox>
-						<b-form-checkbox value="notification-write">{{ $t('notification-write') }}</b-form-checkbox>
+						<b-form-checkbox value="account-read">{{ $t('@.permissions.account-read') }}{{ '(account-read) '}}</b-form-checkbox>
+						<b-form-checkbox value="account-write">{{ $t('@.permissions.account-write') }}{{ '(account-write)'}}</b-form-checkbox>
+						<b-form-checkbox value="note-write">{{ $t('@.permissions.note-write') }}{{ '(note-write)'}}</b-form-checkbox>
+						<b-form-checkbox value="reaction-read">{{ $t('@.permissions.reaction-read') }}{{ '(reaction-read)'}}</b-form-checkbox>
+						<b-form-checkbox value="reaction-write">{{ $t('@.permissions.reaction-write') }}{{ '(reaction-write)'}}</b-form-checkbox>
+						<b-form-checkbox value="following-read">{{ $t('@.permissions.following-read') }}{{ '(following-read)'}}</b-form-checkbox>
+						<b-form-checkbox value="following-write">{{ $t('@.permissions.following-write') }}{{ '(following-write)'}}</b-form-checkbox>
+						<b-form-checkbox value="drive-read">{{ $t('@.permissions.drive-read') }}{{ '(drive-read)'}}</b-form-checkbox>
+						<b-form-checkbox value="drive-write">{{ $t('@.permissions.drive-write') }}{{ '(drive-write)'}}</b-form-checkbox>
+						<b-form-checkbox value="notification-read">{{ $t('@.permissions.notification-read') }}{{ '(notification-read)'}}</b-form-checkbox>
+						<b-form-checkbox value="notification-write">{{ $t('@.permissions.notification-write') }}{{ '(notification-write)'}}</b-form-checkbox>
+						<b-form-checkbox value="favorite-read">{{ $t('@.permissions.favorite-read') }}{{ '(favorite-read)'}}</b-form-checkbox>
+						<b-form-checkbox value="favorite-write">{{ $t('@.permissions.favorite-write') }}{{ '(favorite-write)'}}</b-form-checkbox>
+						<b-form-checkbox value="messaging-read">{{ $t('@.permissions.messaging-read') }}{{ '(messaging-read)'}}</b-form-checkbox>
+						<b-form-checkbox value="messaging-write">{{ $t('@.permissions.messaging-write') }}{{ '(messaging-write)'}}</b-form-checkbox>
+						<b-form-checkbox value="vote-write">{{ $t('@.permissions.vote-write') }}{{ '(vote-write)'}}</b-form-checkbox>
 					</b-form-checkbox-group>
 				</b-form-group>
 			</b-card>
--- src/server/api/endpoints/app/create.ts
+++ src/server/api/endpoints/app/create.ts
@@ -2,6 +2,7 @@ import rndstr from 'rndstr';
 import $ from 'cafy';
 import App, { pack } from '../../../../models/app';
 import define from '../../define';
+import { unique } from '../../../../prelude/array';
 
 export const meta = {
 	tags: ['app'],
@@ -33,13 +34,52 @@ export default define(meta, async (ps, user) => {
 	// Generate secret
 	const secret = rndstr('a-zA-Z0-9', 32);
 
+	let p = ps.permission;
+
+	p = p.map(x => x
+		// v11 => v10
+		.replace('read:account', 'account-read')
+		.replace('write:account', 'account-write')
+		.replace('read:drive', 'drive-read')
+		.replace('write:drive', 'drive-write')
+		.replace('read:favorites', 'favorite-read')
+		.replace('write:favorites', 'favorite-write')
+		.replace('read:following', 'following-read')
+		.replace('write:following', 'following-write')
+		.replace('read:messaging', 'messaging-read')
+		.replace('write:messaging', 'messaging-write')
+		.replace('write:notes', 'note-write')
+		.replace('read:notifications', 'notification-read')
+		.replace('write:notifications', 'notification-write')
+		.replace('read:reactions', 'reaction-read')
+		.replace('write:reactions', 'reaction-write')
+		.replace('write:votes', 'vote-write')
+	);
+
+	// v10 typos
+	if (p.includes('favorites-read')) p.push('favorite-read');
+	if (p.includes('favorite-read')) p.push('favorites-read');
+	if (p.includes('account/read')) p.push('account-read');
+	if (p.includes('account-read')) p.push('account/read');
+	if (p.includes('account/write')) p.push('account-write');
+	if (p.includes('account-write')) p.push('account/write');
+
+	// 今のv10にはなし
+	// 'note-read', 'vote-read',
+
+	// v11にしかない
+	// 'read:blocks', 'write:blocks', 'read:mutes', 'write:mutes'
+	// pages, groups
+
+	p = unique(p);
+
 	// Create account
 	const app = await App.insert({
 		createdAt: new Date(),
 		userId: user && user._id,
 		name: ps.name,
 		description: ps.description,
-		permission: ps.permission,
+		permission: p,
 		callbackUrl: ps.callbackUrl,
 		secret: secret
 	});
