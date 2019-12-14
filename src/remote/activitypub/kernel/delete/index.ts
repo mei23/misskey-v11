a51df15ef433df42a8f5d5c8c7f9f7679cb024bc
mei23 <m@m544.net>
2019-12-15 04:26:50 +0900

    Refactor Delete

--- src/remote/activitypub/kernel/delete/index.ts
+++ src/remote/activitypub/kernel/delete/index.ts
@@ -1,6 +1,6 @@
 import deleteNote from './note';
 import { IRemoteUser } from '../../../../models/user';
-import { IDelete, getApId, isTombstone } from '../../type';
+import { IDelete, getApId, isTombstone, IObject } from '../../type';
 import { toSingle } from '../../../../prelude/array';
 
 /**
@@ -15,25 +15,32 @@ export default async (actor: IRemoteUser, activity: IDelete): Promise<string> =>
 	let formarType: string | undefined;
 
 	if (typeof activity.object === 'string') {
-		// どうせremote resolveしても消えてるので不明なままにしておく
+		// typeが不明だけど、どうせ消えてるのでremote resolveしない
 		formarType = undefined;
-	} else if (isTombstone(activity.object)) {
-		formarType = toSingle(activity.object.formerType);
 	} else {
-		formarType = toSingle(activity.object.type);
+		const object = activity.object as IObject;
+		if (isTombstone(object)) {
+			formarType = toSingle(object.formerType);
+		} else {
+			formarType = toSingle(object.type);
+		}
 	}
 
 	const uri = getApId(activity.object);
 
+	// type不明でもactorとobjectが同じならばそれはPersonに違いない
+	if (!formarType && actor.uri === uri) {
+		formarType = 'Person';
+	}
+
+	// それでもなかったらおそらくNote
+	if (!formarType) {
+		formarType = 'Note';
+	}
+
 	if (['Note', 'Question', 'Article', 'Audio', 'Document', 'Image', 'Page', 'Video'].includes(formarType)) {
 		return await deleteNote(actor, uri);
 	} else if (['Person', 'Service'].includes(formarType)) {
-		return `Delete Actor is not implanted 1`;
-	} else if (uri === actor.uri) {
-		// formarTypeが不明でも対象がactorと同じならばそれはActorに違いない
-		return `Delete Actor is not implanted 2`;
-	} else {
-		// それでもformarTypeが不明だったらおそらくNote
-		return await deleteNote(actor, uri);
+		return `Delete Actor is not implanted`;
 	}
 };
