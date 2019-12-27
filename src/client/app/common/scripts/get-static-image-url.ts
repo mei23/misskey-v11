d2cfdc12913b42748f6dcfda7bf2eb0246f9d9d4
mei23 <m@m544.net>
2019-12-27 21:43:15 +0900

    fix bug

--- src/client/app/common/scripts/get-static-image-url.ts
+++ src/client/app/common/scripts/get-static-image-url.ts
@@ -2,7 +2,7 @@ import { url as instanceUrl } from '../../config';
 import * as url from '../../../../prelude/url';
 
 export function getStaticImageUrl(baseUrl: string, type?: string, animation?: string): string {
-	const anim = type === 'image/gif' || type === 'image/apng' || (type === 'image/png' && animation !== 'no');
+	const anim = type == null || type === 'image/gif' || type === 'image/apng' || (type === 'image/png' && animation !== 'no');
 
 	if (!anim) return baseUrl;
 	const u = new URL(baseUrl);
