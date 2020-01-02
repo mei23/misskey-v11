3ffc7e25166287d7c6c90cf73b7a905279c32740
mei23 <m@m544.net>
2020-01-03 08:26:54 +0900

    リファクタ

--- src/misc/detect-mine.ts
+++ src/misc/detect-mine.ts
@@ -3,6 +3,16 @@ import * as fileType from 'file-type';
 import checkSvg from '../misc/check-svg';
 const probeImageSize = require('probe-image-size');
 
+const TYPE_OCTET_STREAM = {
+	mime: 'application/octet-stream',
+	ext: null as string
+};
+
+const TYPE_SVG = {
+	mime: 'image/svg+xml',
+	ext: 'svg'
+};
+
 export async function detectMine(path: string) {
 	let type = await detectType(path);
 
@@ -11,18 +21,12 @@ export async function detectMine(path: string) {
 
 		// うまく判定できない画像は octet-stream にする
 		if (!imageSize) {
-			type = {
-				mime: 'application/octet-stream',
-				ext: null
-			};
+			type = TYPE_OCTET_STREAM;
 		}
 
 		// 制限を超えている画像は octet-stream にする
 		if (imageSize.wUnits === 'px' && (imageSize.width > 16383 || imageSize.height > 16383)) {
-			type = {
-				mime: 'application/octet-stream',
-				ext: null
-			};
+			type = TYPE_OCTET_STREAM;
 		}
 	}
 
@@ -33,10 +37,7 @@ async function detectType(path: string) {
 	// Check 0 byte
 	const fileSize = await detectFileSize(path);
 	if (fileSize === 0) {
-		return {
-			mime: 'application/octet-stream',
-			ext: null
-		};
+		return TYPE_OCTET_STREAM;
 	}
 
 	const readable = fs.createReadStream(path);
@@ -46,10 +47,7 @@ async function detectType(path: string) {
 	if (type) {
 		// XMLはSVGかもしれない
 		if (type.mime === 'application/xml' && checkSvg(path)) {
-			return {
-				mime: 'image/svg+xml',
-				ext: 'svg'
-			};
+			return TYPE_SVG;
 		}
 
 		return {
@@ -60,17 +58,11 @@ async function detectType(path: string) {
 
 	// 種類が不明でもSVGかもしれない
 	if (checkSvg(path)) {
-		return {
-			mime: 'image/svg+xml',
-			ext: 'svg'
-		};
+		return TYPE_SVG;
 	}
 
 	// それでも種類が不明なら application/octet-stream にする
-	return {
-		mime: 'application/octet-stream',
-		ext: null
-	};
+	return TYPE_OCTET_STREAM;
 }
 
 async function detectFileSize(path: string) {
