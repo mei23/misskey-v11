247df322aabbd8e8ada1d16ede381ac6de611dc7
mei23 <m@m544.net>
2020-01-06 00:29:33 +0900

    公開範囲アイコンに説明

--- src/client/app/common/views/components/visibility-icon.vue
+++ src/client/app/common/views/components/visibility-icon.vue
@@ -1,33 +1,33 @@
 <template>
 	<div style="display: inline-flex; align-items: center;">
-		<div class="wrap" v-if="visibility == 'public'">
+		<div class="wrap" v-if="visibility == 'public'" :title="$t('@.note-visibility.public')">
 			<fa icon="globe"/>
 		</div>
-		<div class="wrap" v-else-if="visibility == 'home'">
+		<div class="wrap" v-else-if="visibility == 'home'" :title="$t('@.note-visibility.home')">
 			<fa icon="home"/>
 		</div>
-		<div class="wrap" v-else-if="visibility == 'followers'">
+		<div class="wrap" v-else-if="visibility == 'followers'" :title="$t('@.note-visibility.followers')">
 			<fa icon="lock"/>
 		</div>
-		<div class="wrap" v-else-if="visibility == 'specified'">
+		<div class="wrap" v-else-if="visibility == 'specified'" :title="$t('@.note-visibility.specified')">
 			<fa icon="envelope"/>
 		</div>
-		<div class="wrap" v-else-if="visibility == 'local-public'">
+		<div class="wrap" v-else-if="visibility == 'local-public'" :title="$t('@.note-visibility.local-public')">
 			<div><fa icon="globe"/></div>
 			<div class="localOnly"><fa icon="heart"/></div>
 		</div>
-		<div class="wrap" v-else-if="visibility == 'local-home'">
+		<div class="wrap" v-else-if="visibility == 'local-home'" :title="$t('@.note-visibility.local-home')">
 			<div><fa icon="home"/></div>
 			<div class="localOnly"><fa icon="heart"/></div>
 		</div>
-		<div class="wrap" v-else-if="visibility == 'local-followers'">
+		<div class="wrap" v-else-if="visibility == 'local-followers'" :title="$t('@.note-visibility.local-followers')">
 			<div><fa icon="lock"/></div>
 			<div class="localOnly"><fa icon="heart"/></div>
 		</div>
-		<div class="wrap" v-if="visibility == 'once-public'">
+		<div class="wrap" v-if="visibility == 'once-public'" :title="$t('@.note-visibility.once-public')">
 			<fa :icon="faHandHoldingHeart"/>
 		</div>
-		<div class="wrap" v-else-if="visibility == 'once-home'">
+		<div class="wrap" v-else-if="visibility == 'once-home'" :title="$t('@.note-visibility.once-home')">
 			<fa :icon="faHandHoldingHeart"/>
 		</div>
 	</div>
@@ -35,9 +35,11 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import i18n from '../../../i18n';
 import { faHandHoldingHeart } from '@fortawesome/free-solid-svg-icons';
 
 export default Vue.extend({
+	i18n: i18n(),
 	props: {
 		v: {
 			type: String,
