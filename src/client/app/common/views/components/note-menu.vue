948eac22e3a4fa0213a2f2d8bb7069d6561d3a30
mei23 <m@m544.net>
2020-01-09 02:20:51 +0900

    fix

--- src/client/app/common/views/components/note-menu.vue
+++ src/client/app/common/views/components/note-menu.vue
@@ -10,7 +10,7 @@ import i18n from '../../../i18n';
 import { url } from '../../../config';
 import copyToClipboard from '../../../common/scripts/copy-to-clipboard';
 import { faCopy, faEye, faEyeSlash, } from '@fortawesome/free-regular-svg-icons';
-import { faPlaneArrival, faPlaneDeparture, faUserFriends } from '@fortawesome/free-solid-svg-icons';
+import { faPlaneArrival, faUserFriends } from '@fortawesome/free-solid-svg-icons';
 
 export default Vue.extend({
 	i18n: i18n('common/views/components/note-menu.vue'),
