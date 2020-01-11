b0955e67b2e6281591d38f5edadac1be3697e9cc
mei23 <m@m544.net>
2020-01-12 06:35:05 +0900

    refix #593

--- src/client/app/admin/views/index.vue
+++ src/client/app/admin/views/index.vue
@@ -9,7 +9,7 @@
 		@click="navOpend = false"
 		@touchstart="navOpend = false"
 	></div>
-	<nav v-show="navOpend" @click="navOpend = false">
+	<nav v-show="navOpend" @click="navOpend = !isMobile">
 		<div class="mi">
 			<img svg-inline src="../assets/header-icon.svg"/>
 		</div>
