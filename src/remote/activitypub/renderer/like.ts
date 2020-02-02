0b9071869133323ee94916bf97123381c581eba6
mei23 <m@m544.net>
2020-02-03 05:54:10 +0900

    genelize Like

--- src/remote/activitypub/renderer/like.ts
+++ src/remote/activitypub/renderer/like.ts
@@ -2,9 +2,27 @@ import config from '../../../config';
 import { ILocalUser } from '../../../models/user';
 import { INote } from '../../../models/note';
 
-export default (user: ILocalUser, note: INote, reaction: string) => ({
-	type: 'Like',
-	actor: `${config.url}/users/${user._id}`,
-	object: note.uri ? note.uri : `${config.url}/notes/${note._id}`,
-	_misskey_reaction: reaction
-});
+export default (user: ILocalUser, note: INote, reaction: string) => {
+	if (generalMap[reaction]) reaction = generalMap[reaction];
+	return {
+		type: 'Like',
+		actor: `${config.url}/users/${user._id}`,
+		object: note.uri ? note.uri : `${config.url}/notes/${note._id}`,
+		content: reaction,
+		_misskey_reaction: reaction
+	};
+};
+
+const generalMap: Record<string, string> = {
+	'like': '👍',
+	'love': '❤',	// ここに記述する場合は異体字セレクタを入れない
+	'laugh': '😆',
+	'hmm': '🤔',
+	'surprise': '😮',
+	'congrats': '🎉',
+	'angry': '💢',
+	'confused': '😥',
+	'rip': '😇',
+	'pudding': '🍮',
+	'star': '⭐',
+};
