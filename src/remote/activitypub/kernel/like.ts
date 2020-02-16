de88068ebde32551ff9b60bcc11d814ec5e1fdc5
mei23 <m@m544.net>
2020-02-17 02:03:24 +0900

    未取得の投稿に対するLikeも受け入れるように

--- src/remote/activitypub/kernel/like.ts
+++ src/remote/activitypub/kernel/like.ts
@@ -1,12 +1,12 @@
 import { IRemoteUser } from '../../../models/user';
 import { ILike, getApId } from '../type';
 import create from '../../../services/note/reaction/create';
-import { fetchNote } from '../models/note';
+import { resolveNote } from '../models/note';
 
 export default async (actor: IRemoteUser, activity: ILike) => {
 	const targetUri = getApId(activity.object);
 
-	const note = await fetchNote(targetUri);
+	const note = await resolveNote(targetUri).catch(() => null);
 	if (!note) return `skip: target note not found ${targetUri}`;
 
 	await create(actor, note, activity._misskey_reaction || activity.content || activity.name);
