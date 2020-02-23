0b8b1605b0ea50b98daa87329b875bcd47b7fcac
mei23 <m@m544.net>
2020-02-24 00:00:56 +0900

    Refactor shoudMuteThisNote

--- src/misc/should-mute-this-note.ts
+++ src/misc/should-mute-this-note.ts
@@ -1,23 +1,35 @@
 import { oidIncludes } from '../prelude/oid';
 
+/**
+ * 対象のNoteをミュートする必要があるか
+ * @param note 対象のPackedNote
+ * @param mutedUserIds ミュートしているユーザーID
+ * @param hideFromUsers Hide指定のあるユーザーID
+ * @param hideFromHosts Hide指定のあるホスト
+ */
 export default function(note: any, mutedUserIds: string[], hideFromUsers?: string[], hideFromHosts?: string[]): boolean {
+	// ミュートしているユーザーの投稿
 	if (oidIncludes(mutedUserIds, note.userId)) {
 		return true;
 	}
 
+	// ミュートしているユーザーへのリプライ
 	if (note.reply != null && oidIncludes(mutedUserIds, note.reply.userId)) {
 		return true;
 	}
 
+	// ミュートしているユーザーへのRenote/Quote
 	if (note.renote != null && oidIncludes(mutedUserIds, note.renote.userId)) {
 		return true;
 	}
 
+	// Hide指定のユーザー
 	if (hideFromUsers && oidIncludes(hideFromUsers, note.userId)) {
 		return true;
 	}
 
-	if (hideFromHosts && oidIncludes(hideFromHosts, note.user.host)) {
+	// Hide指定のホスト
+	if (hideFromHosts && hideFromHosts.includes(note.user.host)) {
 		return true;
 	}
 
