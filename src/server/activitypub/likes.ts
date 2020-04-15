d48cd2cece5daab6aa1dbe87b384c24a84ea23d3
mei23 <m@m544.net>
2020-04-15 20:30:31 +0900

    Revert "fix totalItems"
    This reverts commit 89d28c50f15efe14251118c3902508f707605ca8.

--- src/server/activitypub/likes.ts
+++ src/server/activitypub/likes.ts
@@ -57,8 +57,6 @@ export default async (ctx: Router.IRouterContext) => {
 	const limit = 20;
 	const partOf = `${config.url}/notes/${note._id}/likes`;
 
-	const totalItems = note.reactionCounts ? Object.values(note.reactionCounts).reduce((a, c) => a + c, 0) : 0;
-
 	if (page) {
 		const query = {
 			noteId: note._id
@@ -95,7 +93,7 @@ export default async (ctx: Router.IRouterContext) => {
 				until_id: untilId
 			})}`,
 			// totalItems
-			totalItems,
+			reactions.length,
 			// items
 			activities,
 			// partOf
@@ -117,6 +115,8 @@ export default async (ctx: Router.IRouterContext) => {
 		setResponseType(ctx);
 	} else {
 		// index page
+		const totalItems = note.reactionCounts ? Object.values(note.reactionCounts).reduce((a, c) => a + c, 0) : 0;
+
 		const rendered = renderOrderedCollection(
 			partOf,	// id
 			totalItems,	// totalItems
