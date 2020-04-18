253e25f1d7b96fe312c905b1cb5fffc443f6562d
mei23 <m@m544.net>
2020-04-18 21:41:23 +0900

    accept/reject followのTSエラー修正

--- src/remote/activitypub/kernel/accept/follow.ts
+++ src/remote/activitypub/kernel/accept/follow.ts
@@ -2,20 +2,20 @@ import * as mongo from 'mongodb';
 import User, { IRemoteUser } from '../../../../models/user';
 import config from '../../../../config';
 import accept from '../../../../services/following/requests/accept';
-import { IFollow } from '../../type';
+import { IFollow, getApId } from '../../type';
 
 export default async (actor: IRemoteUser, activity: IFollow): Promise<string> => {
-	const id = typeof activity.actor == 'string' ? activity.actor : activity.actor.id;
+	const id = getApId(activity.actor);
 
 	if (!id.startsWith(config.url + '/')) {
-		return null;
+		return `skip: accept target is no a local user.`;
 	}
 
 	const follower = await User.findOne({
 		_id: new mongo.ObjectID(id.split('/').pop())
 	});
 
-	if (follower === null) {
+	if (follower == null) {
 		return `skip: follower not found`;
 	}
 
