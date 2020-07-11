0efab8c3d5be69ec95623b995dfaa0f403420097
mei23 <m@m544.net>
2020-07-11 21:12:30 +0900

    note summaly

--- src/misc/get-note-summary.ts
+++ src/misc/get-note-summary.ts
@@ -22,12 +22,12 @@ const summarize = (note: any): string => {
 
 	// ファイルが添付されているとき
 	if ((note.files || []).length != 0) {
-		summary += ` (${note.files.length}つのファイル)`;
+		summary += ` (${note.files.length}Files)`;
 	}
 
 	// 投票が添付されているとき
 	if (note.poll) {
-		summary += ' (投票)';
+		summary += ' (Poll)';
 	}
 
 	// 返信のとき
