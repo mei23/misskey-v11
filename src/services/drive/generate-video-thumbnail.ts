a5f50b44d929caf76b4acf6bfa7c520fedd99793
mei23 <m@m544.net>
2020-07-17 21:18:09 +0900

    video thumb cleanup

--- src/services/drive/generate-video-thumbnail.ts
+++ src/services/drive/generate-video-thumbnail.ts
@@ -11,27 +11,29 @@ export async function generateVideoThumbnail(path: string): Promise<IImage> {
 		});
 	});
 
-	await new Promise((res, rej) => {
-		FFmpeg({
-			source: path
-		})
-		.on('end', res)
-		.on('error', rej)
-		.screenshot({
-			folder: outDir,
-			filename: 'output.png',
-			count: 1,
-			timestamps: ['5%']
+	try {
+		await new Promise((res, rej) => {
+			FFmpeg({
+				source: path
+			})
+			.on('end', res)
+			.on('error', rej)
+			.screenshot({
+				folder: outDir,
+				filename: 'output.png',
+				count: 1,
+				timestamps: ['5%']
+			});
 		});
-	});
 
-	const outPath = `${outDir}/output.png`;
+		const outPath = `${outDir}/output.png`;
 
-	const thumbnail = await convertToJpeg(outPath, 530, 255);
+		const thumbnail = await convertToJpeg(outPath, 530, 255);
 
-	// cleanup
-	await fs.promises.unlink(outPath);
-	cleanup();
-
-	return thumbnail;
+		// cleanup
+		await fs.promises.unlink(outPath);
+		return thumbnail;
+	} finally {
+		cleanup();
+	}
 }
