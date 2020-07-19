5d1fe1fc87b2c4af58ab02012abfe6de229517c2
mei23 <m@m544.net>
2020-07-20 01:17:32 +0900

    Fix: 長いカスタム絵文字が認識されない

--- src/misc/extract-emojis.ts
+++ src/misc/extract-emojis.ts
@@ -4,6 +4,6 @@ import { unique } from '../prelude/array';
 
 export default function(mfmForest: MfmForest): string[] {
 	const emojiNodes = preorderF(mfmForest).filter(x => x.type === 'emoji') as EmojiNode[];
-	const emojis = emojiNodes.filter(x => x.props.name && x.props.name.length <= 100).map(x => x.props.name);
+	const emojis = emojiNodes.filter(x => x.props.name && x.props.name.length <= 2048).map(x => x.props.name);
 	return unique(emojis);
 }
