33359d922b2218e72d73851ddf73ebf1dbcb9d64
mei23 <m@m544.net>
2020-12-16 03:17:01 +0900

    通知のリンク先 Resolve #706

--- src/client/app/sw.js
+++ src/client/app/sw.js
@@ -71,3 +71,21 @@ self.addEventListener('push', ev => {
 		});
 	}));
 });
+
+self.addEventListener('notificationclick', function(event) {
+	event.notification.close();
+
+	// This looks to see if the current is already open and
+	// focuses if it is
+	event.waitUntil(clients.matchAll({
+		type: "window"
+	}).then(function(clientList) {
+		for (var i = 0; i < clientList.length; i++) {
+			var client = clientList[i];
+			if (client.url == '/' && 'focus' in client)
+				return client.focus();
+		}
+		if (clients.openWindow)
+			return clients.openWindow('/');
+	}));
+});
