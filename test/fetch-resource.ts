c56b10012a1942ab0e0697528dc246b4252356fb
mei23 <m@m544.net>
2021-01-31 16:15:09 +0900

    Tests for Fetch resource

--- test/fetch-resource.ts
+++ test/fetch-resource.ts
@@ -1,11 +1,11 @@
 /*
- * Tests for launce server
+ * Tests for Fetch resource
  *
  * How to run the tests:
- * > TS_NODE_FILES=true npx mocha test/launch.ts --require ts-node/register
+ * > TS_NODE_FILES=true npx mocha test/fetch-resource.ts --require ts-node/register
  *
  * To specify test:
- * > TS_NODE_FILES=true npx mocha test/launch.ts --require ts-node/register -g 'test name'
+ * > TS_NODE_FILES=true npx mocha test/fetch-resource.ts --require ts-node/register -g 'test name'
  */
 
 process.env.NODE_ENV = 'test';
@@ -14,7 +14,7 @@ import * as assert from 'assert';
 import * as childProcess from 'child_process';
 import { async, launchServer, api, simpleGet  } from './utils';
 
-describe('Launch server', () => {
+describe('Fetch resource', () => {
 	let p: childProcess.ChildProcess;
 
 	before(launchServer(g => p = g, async () => {
@@ -43,6 +43,11 @@ describe('Launch server', () => {
 
 	it('GET api-doc', async(async () => {
 		const res = await simpleGet('/api-doc', 'text/html');
+		assert.strictEqual(res.status, 200);
+	}));
+
+	it('GET api.json', async(async () => {
+		const res = await simpleGet('/api.json', 'application/json');
 		assert.strictEqual(res.status, 200);
 	}));
 });
