de9d14abd4d014ca7696c5af4e40843da443b576
mei23 <m@m544.net>
2021-06-28 23:09:16 +0900

    ap-request

--- src/misc/fetch.ts
+++ src/misc/fetch.ts
@@ -1,25 +1,53 @@
 import * as http from 'http';
 import * as https from 'https';
-import * as cache from 'lookup-dns-cache';
-import fetch, { HeadersInit } from 'node-fetch';
+import CacheableLookup from 'cacheable-lookup';
+import fetch from 'node-fetch';
 import { HttpProxyAgent } from 'http-proxy-agent';
 import { HttpsProxyAgent } from 'https-proxy-agent';
 import config from '../config';
 import { AbortController } from 'abort-controller';
+import Logger from '../services/logger';
 
-export async function getJson(url: string, accept = 'application/json, */*', timeout = 10000, headers?: HeadersInit) {
-	const controller = new AbortController();
-	setTimeout(() => {
-		controller.abort();
-	}, 60 * 1000);
+const logger = new Logger('fetch');
 
-	const res = await fetch(url, {
-		headers: Object.assign({
+export async function getJson(url: string, accept = 'application/json, */*', timeout = 10000, headers?: Record<string, string>) {
+	const res = await getResponse({
+		url,
+		method: 'GET',
+		headers: objectAssignWithLcKey({
 			'User-Agent': config.userAgent,
 			Accept: accept
 		}, headers || {}),
+		timeout
+	});
+
+	try {
+		return await res.json();
+	} catch (e) {
+		throw {
+			name: `JsonParseError`,
+			statusCode: 481,
+			message: `JSON parse error ${e.message || e}`
+		};
+	}
+}
+
+export async function getResponse(args: { url: string, method: string, body?: string, headers: Record<string, string>, timeout?: number, size?: number }) {
+	logger.debug(`${args.method.toUpperCase()} ${args.url}\nHeaders: ${JSON.stringify(args.headers, null, 2)}${args.body ? `\n${args.body}` : ''}`);
+
+	const timeout = args?.timeout || 10 * 1000;
+
+	const controller = new AbortController();
+	setTimeout(() => {
+		controller.abort();
+	}, timeout * 6);
+
+	const res = await fetch(args.url, {
+		method: args.method,
+		headers: args.headers,
+		body: args.body,
 		timeout,
-		size: 10 * 1024 * 1024,
+		size: args?.size || 10 * 1024 * 1024,
 		agent: getAgentByUrl,
 		signal: controller.signal,
 	});
@@ -28,12 +56,30 @@ export async function getJson(url: string, accept = 'application/json, */*', tim
 		throw {
 			name: `StatusError`,
 			statusCode: res.status,
+			statusMessage: res.statusText,
 			message: `${res.status} ${res.statusText}`,
 		};
 	}
 
-	return await res.json();
+	return res;
 }
+
+function lcObjectKey(src: Record<string, string>) {
+	const dst: Record<string, string> = {};
+	for (const key of Object.keys(src).filter(x => x != '__proto__' && typeof src[x] === 'string')) dst[key.toLowerCase()] = src[key];
+	return dst;
+}
+
+function objectAssignWithLcKey(a: Record<string, string>, b: Record<string, string>) {
+	return Object.assign(lcObjectKey(a), lcObjectKey(b));
+}
+
+//#region Agent
+const cache = new CacheableLookup({
+	maxTtl: 3600,	// 1hours
+	errorTtl: 30,	// 30secs
+	lookup: false,	// nativeのdns.lookupにfallbackしない
+});
 
 /**
  * Get http non-proxy agent
@@ -41,7 +87,7 @@ export async function getJson(url: string, accept = 'application/json, */*', tim
 const _http = new http.Agent({
 	keepAlive: true,
 	keepAliveMsecs: 30 * 1000,
-	lookup: cache.lookup,
+	lookup: cache.lookup,	// DefinitelyTyped issues
 } as http.AgentOptions);
 
 /**
@@ -51,7 +97,7 @@ const _https = new https.Agent({
 	keepAlive: true,
 	keepAliveMsecs: 30 * 1000,
 	lookup: cache.lookup,
-});
+} as https.AgentOptions);
 
 /**
  * Get http proxy or non-proxy agent
@@ -72,10 +118,11 @@ export const httpsAgent = config.proxy
  * @param url URL
  * @param bypassProxy Allways bypass proxy
  */
-export function getAgentByUrl(url: URL, bypassProxy = false) {
+export function getAgentByUrl(url: URL, bypassProxy = false): http.Agent | https.Agent {
 	if (bypassProxy) {
 		return url.protocol == 'http:' ? _http : _https;
 	} else {
 		return url.protocol == 'http:' ? httpAgent : httpsAgent;
 	}
 }
+//#endregion Agent
