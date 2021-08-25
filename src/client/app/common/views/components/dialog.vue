3f82a548c60a98593a10aeb8f91b0bc936c2dcf6
mei23 <m@m544.net>
2021-08-25 16:25:34 +0900

    ダイアログのセキュリティ強化

--- src/client/app/common/views/components/dialog.vue
+++ src/client/app/common/views/components/dialog.vue
@@ -23,9 +23,9 @@
 				<fa :icon="faQuestionCircle" v-if="type === 'question'"/>
 				<fa icon="spinner" pulse v-if="type === 'waiting'"/>
 			</div>
-			<header v-if="title" v-html="title"></header>
+			<header v-if="title">{{ title }}</header>
 			<header v-if="title == null && user">{{ $t('@.enter-username') }}</header>
-			<div class="body" v-if="text" v-html="text"></div>
+			<div class="body" v-if="text">{{ text }}</div>
 			<ui-input v-if="input" v-model="inputValue" autofocus :type="input.type || 'text'" :placeholder="input.placeholder" @keydown="onInputKeydown"></ui-input>
 			<ui-input v-if="user" v-model="userInputValue" autofocus @keydown="onInputKeydown"><template #prefix>@</template></ui-input>
 			<ui-select v-if="select" v-model="selectedValue" autofocus>
