d924e591bc4ab5523b4c9ded1f66a9d742a6244a
mei23 <m@m544.net>
2021-08-26 07:24:44 +0900

    Fix integration cookie

--- src/client/app/common/views/components/settings/integration.vue
+++ src/client/app/common/views/components/settings/integration.vue
@@ -54,11 +54,7 @@ export default Vue.extend({
 	},
 
 	mounted() {
-		if (!document.cookie.match(/i=(\w+)/)) {
-			document.cookie = `i=${this.$store.state.i.token}; path=/;` +
-			` domain=${document.location.hostname}; max-age=31536000;` +
-			(document.location.protocol.startsWith('https') ? ' secure' : '');
-		}
+		document.cookie = `i=${this.$store.state.i.token};path=/;${(document.location.protocol.startsWith('https') ? ' secure' : '')}`;
 		this.$watch('$store.state.i', () => {
 			if (this.$store.state.i.twitter) {
 				if (this.twitterForm) this.twitterForm.close();
