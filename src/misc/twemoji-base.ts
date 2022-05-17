adf1aed863a69c1266be298cbb7b5aa4f36e179a
mei23 <m@m544.net>
2022-05-18 08:12:46 +0900

    Update Twemoji CDN

--- src/misc/twemoji-base.ts
+++ src/misc/twemoji-base.ts
@@ -2,4 +2,4 @@
 // export const twemojiSvgBase = 'https://twemoji.maxcdn.com/v/latest/svg';
 
 // Alternative (Fast, with IPv6)
-export const twemojiSvgBase = 'https://cdn.jsdelivr.net/npm/@infrastatic.com/twemoji@14.0.2/svg';
+export const twemojiSvgBase = 'https://cdn.jsdelivr.net/npm/@discordapp/twemoji@14.0.2/dist/svg';
