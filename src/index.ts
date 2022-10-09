f6d02c08c3b259031249f5bfcd347cc05953aad5
MeiMei <30769358+mei23@users.noreply.github.com>
2022-10-10 01:42:56 +0900

    Adjust UV_THREADPOOL_SIZE (#2166)
    * Adjust UV_THREADPOOL_SIZE
    
    * Respect ENV

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@
 Error.stackTraceLimit = Infinity;
 
 require('events').EventEmitter.defaultMaxListeners = 128;
+process.env.UV_THREADPOOL_SIZE = process.env.UV_THREADPOOL_SIZE || `${Math.min(Math.max(4, require('os').cpus().length), 1024)}`;
 
 import boot from './boot';
 
