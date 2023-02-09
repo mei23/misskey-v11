2328e897c4d01cde7a4c4bebbfbff5b4a19133f1
mei23 <m@m544.net>
2023-02-09 09:33:02 +0900

    RSSウィジェットがうごかないのを修正 Fix #4457

--- src/server/web/index.ts
+++ src/server/web/index.ts
@@ -34,7 +34,7 @@ const csp
 	+ `font-src 'self'; `
 	+ `frame-src 'self' https:; `
 	+ `manifest-src 'self'; `
-	+ `connect-src 'self' data: blob: ${config.wsUrl}; `	// wssを指定しないとSafariで動かない https://github.com/w3c/webappsec-csp/issues/7#issuecomment-1086257826
+	+ `connect-src 'self' data: blob: ${config.wsUrl} https://api.rss2json.com; `	// wssを指定しないとSafariで動かない https://github.com/w3c/webappsec-csp/issues/7#issuecomment-1086257826
 	+ `frame-ancestors 'none'`;
 
 // Init app
