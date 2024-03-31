27e84ef308f16507fb2a1a0d53acd6ed90946a1a
mei23 <m@m544.net>
2024-03-31 17:01:45 +0900

    Unicode Emoji 15.1

--- src/misc/emoji-regex.ts
+++ src/misc/emoji-regex.ts
@@ -1,3 +1,3 @@
-const twemojiRegex = require('twemoji-parser/dist/lib/regex').default;
+const twemojiRegex = require('@twemoji/parser/dist/lib/regex').default;
 
 export const emojiRegex = new RegExp(`(${twemojiRegex.source})`);
--- src/misc/emojilist.ts
+++ src/misc/emojilist.ts
@@ -4,4 +4,5 @@ export const emojilist = require('../emojilist.json') as {
 	keywords: string[];
 	char: string;
 	category: 'people' | 'animals_and_nature' | 'food_and_drink' | 'activity' | 'travel_and_places' | 'objects' | 'symbols' | 'flags';
+	v?: string;
 }[];
