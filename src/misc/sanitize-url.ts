aa05b6fb861d57afdb21b1b06afb6fd002c1e0c0
mei23 <m@m544.net>
2024-03-31 16:26:05 +0900

    Improve sanitizeUrl

--- src/misc/sanitize-url.ts
+++ src/misc/sanitize-url.ts
@@ -1,5 +1,7 @@
-export function sanitizeUrl(str: string | null | undefined): string | null | undefined {
+export function sanitizeUrl(str: unknown): string | null | undefined {
 	if (str == null) return str;
+	if (typeof str !== 'string') return null;
+
 	try {
 		const u = new URL(str);
 		if (u.protocol === 'https:') return str;
