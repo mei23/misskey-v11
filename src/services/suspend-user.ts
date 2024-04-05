423ee9ffccf922ce7bc8c172c37d384a963c3e50
mei23 <m@m544.net>
2024-04-05 12:30:31 +0900

    全sharedInbox配信の最適化

--- src/services/suspend-user.ts
+++ src/services/suspend-user.ts
@@ -4,31 +4,60 @@ import { deliver } from '../queue';
 import config from '../config';
 import { User } from '../models/entities/user';
 import { Users, Followings } from '../models';
-import { Not, IsNull } from 'typeorm';
+import { Not, IsNull, Brackets, SelectQueryBuilder } from 'typeorm';
+import { ReadStream } from 'typeorm/platform/PlatformTools';
 
 export async function doPostSuspend(user: User) {
 	if (Users.isLocalUser(user)) {
 		// 知り得る全SharedInboxにDelete配信
 		const content = renderActivity(renderDelete(`${config.url}/users/${user.id}`, user));
 
-		const queue: string[] = [];
+		const query = Followings.createQueryBuilder('following')
+			.select('distinct coalesce(following.followerSharedInbox, following.followeeSharedInbox) as inbox') 
+			.where(new Brackets((qb) =>
+				qb.where({ followerHost: Not(IsNull()) })
+				.orWhere({ followeeHost: Not(IsNull()) })
+			))
+			.andWhere(new Brackets((qb) => 
+				qb.where({ followerSharedInbox: Not(IsNull()) })
+				.orWhere({ followeeSharedInbox: Not(IsNull()) })
+			));
 
-		const followings = await Followings.find({
-			where: [
-				{ followerSharedInbox: Not(IsNull()) },
-				{ followeeSharedInbox: Not(IsNull()) }
-			],
-			select: ['followerSharedInbox', 'followeeSharedInbox']
+		for (const row of await query.getRawMany()) {
+			deliver(user as any, content, row.inbox);
+		}
+
+		/* streaming ここまでいらないと思うが
+		async function ProcessStreamingRow<T> (query: SelectQueryBuilder<T>, callback: (row: Record<string, unknown>) => Promise<void>) {
+			return new Promise(async (res, rej) => {
+				// query and get stream
+				let stream: ReadStream;
+				try {
+					stream = await query.stream();
+				} catch (e) {
+					return rej(e);
+				}
+
+				stream
+					.on('data', async (data: any) => {	// Buffer | string のはずだけどobjectが返ってくる
+						try {
+							await callback(data);
+						} catch (e) {
+							rej(e);
+						}
+					})
+					.on('end', () => res('end'))
+					.on('error', err => rej(err));
+			});
+		}
+
+		await ProcessStreamingRow(query, async row => {
+			if (typeof row.inbox === 'string') {
+				deliver(user as any, content, row.inbox);
+			} else {
+				console.warn('nnn');
+			}
 		});
-
-		const inboxes = followings.map(x => x.followerSharedInbox || x.followeeSharedInbox);
-
-		for (const inbox of inboxes) {
-			if (inbox != null && !queue.includes(inbox)) queue.push(inbox);
-		}
-
-		for (const inbox of queue) {
-			deliver(user as any, content, inbox);
-		}
+		*/
 	}
 }
